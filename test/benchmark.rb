d573cfe4dca33335de15c6f239ad4aff0f541b56
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-05-23 16:30:44 +0200

    Do not shuffle arrays in benchmarks
    This is needed to have reproducible runs

--- test/benchmark.rb
+++ test/benchmark.rb
@@ -9,8 +9,8 @@ require 'benchmark'
 row = []
 input1 = (32..126).to_a.pack('U*').chars.to_a # these will need to be escaped
 input2 = (65..122).to_a.pack('U*').chars.to_a # these do not need to be escaped
-10.times { row << input1.shuffle.join }
-10.times { row << input2.shuffle.join }
+10.times { row << input1.join }
+10.times { row << input2.join }
 times = 3000
 
 Benchmark.bmbm(30) do |x|
